fe4acc89aebf2a6e1e59d1e825a32dd3d25a536b
esthete88 <nordmtr@gmail.com>
2019-07-13 00:29:02 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setup(
+    name='quantpy',
+    version='0.1',
+    description='Framework for quantum computations and tomography',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Dmitry Norkin, Anton Bozhedarov',
+    author_email='nordmtr@gmail.com',
+    requires=[
+        'numpy',
+        'scipy',
+        'sys'
+    ],
+    packages=setuptools.find_packages(),
+    package_dir=setuptools.find_packages()
+)
