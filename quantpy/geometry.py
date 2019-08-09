f1ebbbda0c1709458db8759f324828b4e97a5795
esthete88 <nordmtr@gmail.com>
2019-08-09 19:40:01 +0300

    linear inversion tomography

--- quantpy/geometry.py
+++ quantpy/geometry.py
@@ -4,7 +4,11 @@ import scipy.linalg as la
 
 def hs_dst(A, B):
     """Return Hilbert-Schmidt distance between two matrices"""
-    dist = np.sqrt(abs(np.trace((A - B) * (A - B)))) / np.sqrt(2)
+    if not isinstance(A, np.ndarray):
+        A = A.matrix
+    if not isinstance(B, np.ndarray):
+        B = B.matrix
+    dist = np.sqrt(abs(np.trace((A - B) @ (A - B)))) / np.sqrt(2)
     if dist < 1e-15:
         return 0
     else:
@@ -13,6 +17,10 @@ def hs_dst(A, B):
 
 def trace_dst(A, B):
     """Return trace distance between two matrices"""
+    if not isinstance(A, np.ndarray):
+        A = A.matrix
+    if not isinstance(B, np.ndarray):
+        B = B.matrix
     dist = abs(np.trace(la.sqrtm((A - B) @ (A - B)))) / 2
     if dist < 1e-15:
         return 0
@@ -22,6 +30,10 @@ def trace_dst(A, B):
 
 def if_dst(A, B):
     """Return infidelity between two matrices"""
+    if not isinstance(A, np.ndarray):
+        A = A.matrix
+    if not isinstance(B, np.ndarray):
+        B = B.matrix
     dist = 1 - np.abs(np.trace(la.sqrtm(la.sqrtm(A) @ B @ la.sqrtm(A))) ** 2)
     if dist < 1e-15:
         return 0
--- quantpy/tomography.py
+++ quantpy/tomography.py
@@ -2,28 +2,21 @@ import numpy as np
 import scipy.linalg as la
 from scipy.optimize import minimize
 
-from .geometry import hs_dst, if_dst, trace_dst, product
+from .geometry import hs_dst, if_dst, trace_dst
 from .qobj import Qobj
 from .measurements import generate_measurement_matrix
+from .routines import _left_inv
 
 
 # TODO: fix bloch vector
 def _is_feasible(bloch_vector):
     """Constraint for minimize function"""
-    return 1 - la.norm(bloch_vector, ord=2)
+    return 1 - la.norm(bloch_vector[1:], ord=2)
 
 
 class Tomograph:
     """Basic class for QST"""
     def __init__(self, state, dst='hs'):
-        """
-        Initialize quantum state tomography procedure with the preferred set_of_POVMs
-        Input:
-            set_of_POVMs -- like in state_to_proba function (list of POVMs)
-        """
-        self.POVM_matrix = np.empty((0, 4 ** state.dim))
-        self.results = []
-        self.n_measurements = 0
         self.state = state
         if isinstance(dst, str):
             if dst == 'hs':
@@ -37,17 +30,21 @@ class Tomograph:
         else:
             self.dst = dst
 
-    def experiment(self, n_measurements, POVM='mub', warm_start=False):
-        POVM_matrices = generate_measurement_matrix(POVM, self.state.dim)
-        if isinstance(n_measurements, int):
-            n_measurements = [n_measurements] * len(POVM_matrices)
-        for POVM_matrix, n_measurements_elem in zip(POVM_matrices, n_measurements):
-            self._experiment(POVM_matrix, n_measurements_elem)
-
-    def _experiment(self, POVM_matrix, n_measurements):
-        probas = POVM_matrix @ self.state.bloch
-        self.results.append(np.random.multinomial(n_measurements, probas))
-        self.n_measurements += n_measurements
+    def experiment(self, n_measurements, POVM='proj', warm_start=False):
+        POVM_matrix = generate_measurement_matrix(POVM, self.state.dim)
+        probas = POVM_matrix @ self.state.bloch * (2 ** self.state.dim)
+        results = np.random.multinomial(n_measurements, probas)
+        if warm_start:
+            self.POVM_matrix = np.vstack((
+                self.POVM_matrix * self.n_measurements,
+                POVM_matrix * n_measurements,
+            )) / (self.n_measurements + n_measurements)
+            self.results = np.hstack((self.results, results))
+            self.n_measurements += n_measurements
+        else:
+            self.POVM_matrix = POVM_matrix
+            self.results = results
+            self.n_measurements = n_measurements
 
     def point_estimate(self, method='lin', physical=True):
         if method == 'lin':
@@ -59,7 +56,25 @@ class Tomograph:
         pass
 
     def _point_estimate_lin(self, physical):
-        pass
+        frequencies = self.results / self.results.sum()
+        bloch_vec = _left_inv(self.POVM_matrix) @ frequencies / (2 ** self.state.dim)  # norm coef
+        if physical and la.norm(bloch_vec[1:], ord=2) > 1:
+            bloch_vec[1:] /= la.norm(bloch_vec[1:], ord=2)
+        return Qobj(bloch_vec)
 
     def _point_estimate_mle(self, physical=True):
-        pass
+        constraints = [
+            {'type': 'ineq', 'fun': _is_feasible},
+        ]
+        # x0 = Qobj(SIGMA_I / 2).bloch
+        x0 = self.point_estimate('lin').bloch
+        opt_res = minimize(self._likelihood, x0, constraints=constraints, method='SLSQP')
+        bloch_vec = opt_res.x
+        if physical and la.norm(bloch_vec, ord=2) > 1:
+            bloch_vec /= la.norm(bloch_vec, ord=2)
+        return Qobj(bloch_vec)
+
+    def _likelihood(self, bloch_vec, eps=1e-8):
+        rho = Qobj(bloch_vec)
+        likelihood = 0
+        return -likelihood
