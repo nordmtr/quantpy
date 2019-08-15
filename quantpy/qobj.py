51bc76354d445a54189f4f17278643f0b635e1b0
esthete88 <nordmtr@gmail.com>
2019-08-15 17:40:40 +0300

    Qobj docs

--- quantpy/qobj.py
+++ quantpy/qobj.py
@@ -2,14 +2,74 @@ import sys
 import numpy as np
 import scipy.linalg as la
 
+from copy import deepcopy
+
 from .geometry import product
 from .routines import generate_pauli, _density
 
 
 class Qobj:
-    """Basic class for quantum objects."""
+    """Basic class for representing quantum objects, such as quantum states and operators
+
+    This class supports all simple math operations, as well as a collection of common
+    state/operator operations.
+
+    Parameters
+    ----------
+    data : array-like
+        If `data` is 2-D, it is treated as a full matrix
+        If `data` is 1-D and `is_ket` is False, it is treated as a bloch vector
+        If `data` is 1-D and `is_let` is True, it is treated as a ket vector
+    is_ket : bool
+
+    Attributes
+    ----------
+    bloch : numpy 1-D array (property)
+        A vector, representing the quantum object in Pauli basis (only for Hermitian matrices)
+    dim : int
+        Number of qubits
+    H : Qobj (property)
+        Adjoint matrix of the quantum object
+    matrix : numpy 2-D array (property)
+        Quantum object in a matrix form
+    T : Qobj (property)
+        Transpose of the quantum object
+
+    Methods
+    -------
+    copy()
+        Create a copy of this Qobj instance
+    conj()
+        Conjugate of the quantum object
+    eig()
+        Eigenvalues and eigenvectors of the quantum object
+    is_density_matrix()
+        Check if the quantum object is valid density matrix
+    is_pure()
+        Check if the quantum object is rank-1 valid density matrix
+    tensordot()
+        Kronecker product of 2 Qobj instances
+    trace()
+        Trace of the quantum object
+
+    Examples
+    --------
+    >>> qp.Qobj([0.5, 0, 0, 0.5])
+    array([[1.+0.j, 0.+0.j],
+           [0.+0.j, 0.+0.j]])
+
+    >>> qp.Qobj([[1.+0.j, 0.+0.j],
+                 [0.+0.j, 0.+0.j]])
+    array([[1.+0.j, 0.+0.j],
+           [0.+0.j, 0.+0.j]])
+
+    >>> qp.Qobj([1, 0], is_ket=True)
+    array([[1.+0.j, 0.+0.j],
+           [0.+0.j, 0.+0.j]])
+    """
 
     def __init__(self, data=None, is_ket=False):
+
         self._types = set()  # Set of types which represent the state
         if is_ket:
             data = _density(data)
@@ -29,6 +89,7 @@ class Qobj:
 
     @property
     def matrix(self):
+        """Quantum object in a matrix form"""
         if 'matrix' not in self._types:
             self._types.add('matrix')
             basis = generate_pauli(self.dim)
@@ -46,6 +107,7 @@ class Qobj:
 
     @property
     def bloch(self):
+        """A vector, representing the quantum object in Pauli basis"""
         if 'bloch' not in self._types:
             self._types.add('bloch')
             basis = generate_pauli(self.dim)
@@ -61,11 +123,51 @@ class Qobj:
         self._type = 'bloch'
         self._bloch = data
 
+    @property
+    def T(self):
+        """Transpose of the quantum object"""
+        return self.__class__(self.matrix.T)
+
+    @property
+    def H(self):
+        """Adjoint matrix of the quantum object"""
+        return self.__class__(self.matrix.T.conj())
+
+    def conj(self):
+        """Conjugate of the quantum object"""
+        return self.__class__(self.matrix.conj())
+
+    def trace(self):
+        """Trace of the quantum object"""
+        return np.trace(self.matrix)
+
+    def eig(self):
+        """Find eigenvalues and eigenvectors of the quantum object
+
+        Returns
+        -------
+        v : complex numpy 1-D array
+            The eigenvalues, each repeated according to its multiplicity
+        U : complex numpy 2-D array
+            The normalized right eigenvector corresponding to the eigenvalue `v[i]`
+            is the column `U[:, i]`
+
+        Raises
+        ------
+        LinAlgError
+            If eigenvalue computation does not converge
+        """
+        return la.eig(self.matrix)
+
     def tensordot(self, other):
-        """Return Kronecker product of 2 Qobj instances."""
+        """Kronecker product of 2 Qobj instances. Returns another Qobj instance."""
         return self.__class__(np.kron(self.matrix, other.matrix))
 
     def is_density_matrix(self):
+        """Check if the quantum object is a valid density matrix.
+        Perform a test for hermiticity, positive semi-definiteness and unit trace.
+        Alert the user about violations of the specific properties.
+        """
         herm_flag = np.allclose(self.matrix, self.matrix.T.conj())
         pos_flag = np.allclose(np.minimum(np.real(la.eigvals(self.matrix)), 0), 0)
         trace_flag = np.allclose(np.trace(self.matrix), 1)
@@ -80,7 +182,12 @@ class Qobj:
         return False
 
     def is_pure(self):
-        return np.linalg.matrix_rank(self.matrix, tol=1e-10, hermitian=True) == 1
+        """Check if the quantum object is a valid rank-1 density matrix"""
+        return (np.linalg.matrix_rank(self.matrix, tol=1e-10, hermitian=True) == 1) and self.is_density_matrix()
+
+    def copy(self):
+        """Create a copy of this Qobj instance"""
+        return deepcopy(self)
 
     def __repr__(self):
         return repr(self.matrix)
