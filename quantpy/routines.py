1ef6febca9d56c501953ee216507983dcd9af914
esthete88 <nordmtr@gmail.com>
2019-08-27 14:11:31 +0300

    Hide raw pauli

--- quantpy/routines.py
+++ quantpy/routines.py
@@ -4,19 +4,19 @@ import scipy.linalg as la
 
 # Pauli basis
 
-SIGMA_I = np.array([[1, 0], [0, 1]], dtype=np.complex128)
-SIGMA_X = np.array([[0, 1], [1, 0]], dtype=np.complex128)
-SIGMA_Y = np.array([[0, -1j], [1j, 0]], dtype=np.complex128)
-SIGMA_Z = np.array([[1, 0], [0, -1]], dtype=np.complex128)
+_SIGMA_I = np.array([[1, 0], [0, 1]], dtype=np.complex128)
+_SIGMA_X = np.array([[0, 1], [1, 0]], dtype=np.complex128)
+_SIGMA_Y = np.array([[0, -1j], [1j, 0]], dtype=np.complex128)
+_SIGMA_Z = np.array([[1, 0], [0, -1]], dtype=np.complex128)
 
-PAULI_1 = [SIGMA_I, SIGMA_X, SIGMA_Y, SIGMA_Z]
+_PAULI_1 = [_SIGMA_I, _SIGMA_X, _SIGMA_Y, _SIGMA_Z]
 
 
 def generate_pauli(dim):
     """Generate basis of Pauli matrices for `dim` qubits"""
-    basis = PAULI_1
+    basis = _PAULI_1
     for _ in range(dim - 1):
-        basis = np.kron(basis, PAULI_1)
+        basis = np.kron(basis, _PAULI_1)
     return basis
 
 
