6acc87fbd2d47f245a2f8b56fd204e055c198d81
esthete88 <nordmtr@gmail.com>
2019-08-27 14:10:31 +0300

    Finished channel class

--- quantpy/channel.py
+++ quantpy/channel.py
@@ -3,6 +3,7 @@ import numpy as np
 from .base_quantum import BaseQuantum
 from .routines import generate_single_entries
 from .qobj import Qobj
+from .routines import kron
 
 
 class Channel(BaseQuantum):
@@ -15,8 +16,8 @@ class Channel(BaseQuantum):
             if dim is None:
                 raise ValueError('`dim` argument is compulsory when using init with function')
             self.dim = dim
-        elif isinstance(data, np.ndarray):
-            self._choi = data
+        elif isinstance(data, np.ndarray) or isinstance(data, Qobj):
+            self._choi = Qobj(data)
             self._func = None
             self._types.add('choi')
             self.dim = int(np.log2(data.shape[0]) / 2)
@@ -39,18 +40,29 @@ class Channel(BaseQuantum):
     def choi(self):
         if 'choi' not in self._types:
             self._types.add('choi')
-            self._choi = np.array((4 ** self.dim, 4 ** self.dim), dtype=np.complex128)
-            for single_entry in generate_single_entries(4 ** self.dim):
-                self._choi += np.kron(single_entry, self.transform(single_entry))
+            self._choi = Qobj(np.zeros((4 ** self.dim, 4 ** self.dim), dtype=np.complex128))
+            for single_entry in generate_single_entries(2 ** self.dim):
+                self._choi += kron(Qobj(single_entry), self.transform(single_entry))
         return self._choi
 
     @choi.setter
     def choi(self, data):
         self._types.add('choi')
         self._types.discard('func')
+        if not isinstance(data, Qobj):
+            data = Qobj(data)
         self._choi = data
         self.dim = int(np.log2(data.shape[0]) / 2)
 
     def transform(self, state):
-        common_state = state.T.tensordot(Qobj(np.eye(2 ** self.dim)))
-        return common_state.ptrace(list(range(self.dim, 2 * self.dim)))
+        if not isinstance(state, Qobj):
+            state = Qobj(state)
+        if 'func' in self._types:
+            output_state = self._func(state)
+        else:  # compute output state using Choi matrix
+            common_state = kron(state.T, Qobj(np.eye(2 ** self.dim)))
+            output_state = (common_state @ self.choi).ptrace(list(range(self.dim, 2 * self.dim)))
+        return output_state
+
+    def __repr__(self):
+        return 'Quantum channel w Choi matrix\n' + repr(self.choi.matrix)
